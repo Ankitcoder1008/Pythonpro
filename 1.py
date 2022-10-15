e84547fb79b9a3daea5c30d8e2280f56f887cfe8
Ankitcoder1008 <115339749+Ankitcoder1008@users.noreply.github.com>
2022-10-15 13:38:02 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,25 @@
+#arithmetic operator
+print(10+20)#addition
+print(20-10)#substaction
+print(20*20)#multiplication
+print(30/15)#division
+print(20%10)#modulous
+print(20//9)#floordivision its round the result
+#Comparison operator
+print(3==4)#equal
+print(3!=4)#not equal
+print(3>4)
+print(3<4)
+print(3>=4)
+print(3<=4)
+#logical operator
+x=2
+print(x<5 and x<8)
+print(x<5 or x<5)
+print(not(x<5 and x<8))
+#identity operator
+a=[10,20]
+b=[10,20]
+c=a
+print(a is c)
+print(b is c)
